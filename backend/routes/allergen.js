6f40efcf9390ab32daf7ca43aa33a3edc0da3423
SandorStefan62 <sandor.stefanandrei02@gmail.com>
2024-01-15 04:39:38 +0200

    add allergen routes

--- backend/routes/allergen.js
+++ backend/routes/allergen.js
@@ -2,6 +2,7 @@ const express = require('express');
 const moment = require('moment-timezone');
 const { verifyToken } = require('../middlewares/authMiddleware.js');
 const { client } = require('../db.js');
+const { ObjectId } = require('mongodb');
 
 const allergenRouter = express.Router();
 
@@ -34,6 +35,89 @@ allergenRouter.post('/addAllergenZone', verifyToken, async (req, res) => {
 
     } catch (error) {
         console.error(`Error sending new allergen zone: ${error}`);
+        return res.status(500).json({ error: `Internal server error` });
+    }
+});
+
+//GET /allergen/getAllAllergens: retrieve all reported allergen zones
+allergenRouter.get('/getAllAllergens', verifyToken, async (req, res) => {
+    try {
+        const collection = db.collection('allergens');
+
+        const allergens = await collection.find().toArray();
+
+        return res.status(200).json({ allergens });
+    } catch (error) {
+        console.error(`Error retrieving allergen zones: ${error}`);
+        return res.status(500).json({ error: `Internal server error` });
+    }
+});
+
+//GET /allergen/:allergen: retrieve all zones with specific allergen
+allergenRouter.get('/getAllergenZones/:allergen', verifyToken, async (req, res) => {
+    try {
+        const collection = db.collection('allergens');
+        const allergenType = req.params.allergen;
+
+        const allergens = await collection.find({ allergenType: allergenType }).toArray();
+
+        return res.status(200).json({ allergens });
+    } catch (error) {
+        console.error(`Error retrieving allergen zones: ${error}`);
+        return res.status(500).json({ error: `Internal server error` });
+    }
+});
+
+//GET /allergen/:id: retrieve details of a specific allergen zone
+allergenRouter.get('/getSpecificAllergenZone/:id', verifyToken, async (req, res) => {
+    try {
+        const collection = db.collection('allergens');
+        const allergenId = req.params.id;
+
+        const allergen = await collection.findOne({ _id: new ObjectId(allergenId) });
+
+        return res.status(200).json({ allergen });
+    } catch (error) {
+        console.error(`Error retrieving allergen zone: ${error}`);
+        return res.status(500).json({ error: `Internal server error` });
+    }
+});
+
+//PUT /allergen/updateAllergen/:id: update a specific allergen zone data
+allergenRouter.put('/updateAllergen/:id', verifyToken, async (req, res) => {
+    try {
+        const collection = db.collection('allergens');
+        const allergenId = req.params.id;
+        const updatedAllergen = req.body;
+
+        const result = await collection.updateOne({ _id: new ObjectId(allergenId) }, { $set: updatedAllergen });
+
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ error: `Allergen zone not found` });
+        }
+
+        return res.status(200).json({ message: `Allergen zone updated successfully` });
+    } catch (error) {
+        console.error(`Error updating allergen zone: ${error}`);
+        return res.status(500).json({ error: `Internal server errro` });
+    }
+});
+
+//DELETE /:id: delete a specific allergen zone
+allergenRouter.delete('/:id', verifyToken, async (req, res) => {
+    try {
+        const collection = db.collection('allergens');
+        const allergenId = req.params.id;
+
+        const result = await collection.deleteOne({ _id: new ObjectId(allergenId) });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ error: `Allergen zone not found` });
+        }
+
+        return res.status(200).json({ message: `Allergen zone deleted succsessfully` });
+    } catch (error) {
+        console.error(`Error deleting allergen zone: ${error}`);
         return res.status(500).json({ error: `Internal server error` });
     }
 });
